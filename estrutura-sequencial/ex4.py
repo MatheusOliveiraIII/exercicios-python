21d628704435158d885491051c8c819f951c35b2
Matheus Oliveira <matheus23648392@gmail.com>
2021-08-16 14:02:32 -0300

    reposta exercicio5

--- estrutura-sequencial/ex4.py
+++ estrutura-sequencial/ex4.py
@@ -3,6 +3,5 @@ n2 = int(input('Digite as notas: '))
 n3 = int(input('Digite o terceiro bimestre: '))
 n4 = int(input('Pra finalizar as notas do 4 bimestre: '))
 m = n1 + n2 + n3 + n4
-s = n1 * n2 * n3 * n4
 d = n1 / n2 / n3 / n4
-print('A soma é {} produto é {} e a divisao é {}'.format(s, m, d))
+print('O produto é {} e a divisao é {}'.format(m, d))
