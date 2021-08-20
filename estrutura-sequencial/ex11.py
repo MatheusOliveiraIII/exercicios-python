56e75cf0c505c1472e079ecab80f4d389c54c7ba
Matheus Oliveira <matheus23648392@gmail.com>
2021-08-20 14:13:01 -0300

    reposta exercicio 10

---
+++ estrutura-sequencial/ex11.py
@@ -0,0 +1,10 @@
+r1 = int(input('Digite o 1 numero inteiro: '))
+r2 = int(input('Digite o 2 numero inteiro: '))
+r3 = int(input('Digite o 1 numero real: '))
+r1 = r1 * 2
+r2 = r2 / 2
+r5 = r1 + r2
+r9 = r1 * 3
+r6 = r1 + r3
+r8 = r3 * 3
+print('A reposta da letra a foi {} a da letra b {} e a da letra c foi {}!'.format(r5, r6, r8))
